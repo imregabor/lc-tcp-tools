a2c4b2720e28848076fb73d2fa08d1eb0cfb8931
Gabor Imre <imregabor@gmail.com>
2022-09-26 02:32:24 +0200

    Add rendering frame counter

--- replay-demo/src/index.js
+++ replay-demo/src/index.js
@@ -78,6 +78,52 @@ function parsePacket(s) {
 }
 
 
+function addFrameCounter(d3sel) {
+  var shown;
+
+  const div = d3sel.append('div').classed('frame-counter', true);
+
+  div.append('div').classed('frame-label', true).text('frame:');
+  div.append('div').classed('time-label', true).text('time:');
+
+  const fv = div.append('div').classed('frame-value', true);
+  const tv = div.append('div').classed('time-value', true);
+
+  var fc = 0;
+
+  const format = d3.format('04d');
+  const boxes = [];
+
+  for (var i = 0; i < 20; i++) {
+    boxes.push(
+      div.append('div').classed('box', true).style('left', (i * 20 + 170) + 'px')
+    );
+  }
+
+  const ret = {
+    show : show => {
+      shown = show;
+      div.style('display', shown ? 'block' : 'none');
+    },
+    frame : () => {
+      if (!shown) {
+        return;
+      }
+
+      const box = boxes[ fc % boxes.length ];
+      box.classed('on', !box.classed('on'));
+
+      fc = (fc + 1) % 10000;
+      fv.text(format(fc));
+      tv.text(format(Date.now() % 10000));
+
+    }
+  };
+  ret.show(true);
+  return ret;
+
+}
+
 function attachPerfCounter(d3sel, formatSpec) {
   const buf = [];
   const cnt = [];
@@ -367,6 +413,17 @@ function initPage() {
   packetGroupsPerSec = attachPerfCounter(pgpsdiv, ".1f");
 
 
+  const frameCounter = addFrameCounter(body);
+  frameCounter.show(false);
+
+  const pageControls = fpsdiv.append('div').classed('page-controls', true);
+  const frameCounterBtn = pageControls.append('i').classed('fa fa-clock', true).attr('title', 'Show/hide precision frame counter');
+  frameCounterBtn.on('click', () => {
+    const next = !frameCounterBtn.classed('on');
+    frameCounter.show(next);
+    frameCounterBtn.classed('on', next);
+  });
+
 
   m1 = addMatrix(body, { cols: 24, rows: 1, sep: 0.1, pad: 0.2 } )
     .leftLabelText("thujas")
@@ -459,6 +516,7 @@ function initPage() {
     fps.render();
     packetsPerSec.render();
     packetGroupsPerSec.render();
+    frameCounter.frame();
     requestAnimationFrame(r);
 
     // should be in the rendering loop
